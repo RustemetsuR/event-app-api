f18c60d18b884d078f690ebe9947d4ded3bc95a4
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-12-10 17:23:52 +0600

    Added config

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  db: {
+    name: "event-app",
+    url: "mongodb://localhost"
+  },
+  fb: {
+    appId: "1014972402247833",
+    appSecret: "cb623db3dc53e62d10c8c1c91ce2fef4"
+  }
+};
